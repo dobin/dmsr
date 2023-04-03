01188260b40039a89b04a8566916885fa4445b85
Dobin <dobin@broken.ch>
2023-04-03 21:23:40 +0200

    initial commit

---
+++ client/network.py
@@ -0,0 +1,42 @@
+import requests
+import socket
+import logging
+
+
+class Network():
+    def __init__(self):
+        self.hostname = socket.gethostname()
+        self.password = ''
+        self.server = ''
+
+
+    def send(self, data, name, refresh):
+        global CONFIG
+        packet = {
+            'agentname': self.hostname,
+            'pluginname': name,
+            'refresh': refresh,
+            'password': self.password,
+            'data': data,
+        }
+
+        try:
+            res = requests.post(self.server + '/push', json=packet)
+            if res.ok:
+                return True
+            else:
+                logging.warn("Request was not ok")
+        except requests.HTTPError as e:
+            logging.warn("Error sending HTTP request: {}".format(e.response.status_code))
+        except Exception as e:
+            logging.warn("Error: {}".format(e))
+
+        return False
+
+
+    def setConfig(self, server, password):
+        self.server = server
+        self.password = password
+
+
+Network = Network()
