8117d987417f76e1d43cef9b553c5c357faa2a14
Dobin Rutishauser <dobin.rutishauser@gmail.com>
2025-12-16 20:16:16 +0100

    feature: http url with name

--- plugins/http.py
+++ plugins/http.py
@@ -7,19 +7,19 @@ from utils import prettyNumber
 class http(Plugin):
     def __init__(self, refresh):
         super().__init__(refresh)
-        self.config['urls'] = []
+        self.config['urls'] = {}
 
 
     def run(self) -> Tuple[Dict, PluginStatus]:
         data = {}
         status = PluginStatus.OK
 
-        for url in self.config['urls']:
+        for name, url in self.config['urls'].items():
             try:
                 page = requests.get(url, timeout=1)
-                data[url] = 'Up'
+                data[name] = 'Up'
             except:
-                data[url] = 'Down'
+                data[name] = 'Down'
                 status = PluginStatus.WARN
 
         return data, status
